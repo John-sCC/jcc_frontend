e73b19f9ce2eda34e58e38cabb05df2ab5a075ad
Aiden Huynh <ah5993909@gmail.com>
2024-01-18 11:11:03 -0800

    Made icons unselectable

--- _site/assets/js/search.js
+++ _site/assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/sortsortsort/assets/js/search-data.json', true);
+    request.open('GET', '/jcc_frontend/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
