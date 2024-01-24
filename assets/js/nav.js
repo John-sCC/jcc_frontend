7970359193255161c309f5a102680d9a6d7a8999
Aiden Huynh <ah5993909@gmail.com>
2024-01-24 13:36:46 -0800

    Redirects with jQuery in script

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,4 +1,18 @@
+// I don't know why but {{site.baseurl}} isn't working here so this is a temporary fix
+const baseurl = "/jcc_frontend"
+
 $(window).ready(function() {
-    // Select buttons and add onclicks
-    
+    // Redirect buttons to respective pages
+    for (let link of ["classes", "sign-in", "blogs", "about"]) {
+        $(`#${link}`).click( function() { 
+            location.href=`${baseurl}/${link}`
+        })
+    }
+
+    // Redirect logo and title clicks to index
+    for (let home of ['nav-logo', 'nav-title']) {
+        $(`.${home}`).click( function() { 
+            location.href=`${baseurl}`
+        })
+    }
 })
