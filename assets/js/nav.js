3c234fb6aafbe82a497568c775853e38492bda29
Aiden Huynh <ah5993909@gmail.com>
2024-01-25 11:47:46 -0800

    simplified hrefs

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,19 +1,19 @@
 // Temporary fix until we get a custom domain
-const baseurl = "/jcc_frontend"
+const baseurl = "/jcc_frontend/"
 
 // Wait for HTML elements to load
 $(window).ready(function() {
     // Redirect buttons to respective pages
     for (let link of ["classes", "sign-in", "blogs", "about"]) {
         $(`#${link}`).click( function() { 
-            location.href=`${baseurl}/${link}`
+            location.href = baseurl + link
         })
     }
 
     // Redirect logo and title clicks to index
     for (let home of ['nav-logo', 'nav-title']) {
         $(`.${home}`).click( function() { 
-            location.href=`${baseurl}`
+            location.href = baseurl
         })
     }
 })
