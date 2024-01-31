3708f2797e84977e4a65842aeb2bd350b4648db3
Aiden Huynh <ah5993909@gmail.com>
2024-01-31 12:40:01 -0800

    Sticky nav

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,5 +1,7 @@
 // Wait for HTML elements to load
 $(window).ready(function() {
+    var navBottom = $(".nav-bottom")[0]
+
     // Redirect buttons to respective pages
     for (let link of ["classes", "sign-in", "blogs", "about"]) {
         $(`#${link}`).click( function() { 
@@ -12,5 +14,16 @@ $(window).ready(function() {
         $(`.${home}`).click( function() { 
             location.href = `${baseurl}/`
         })
+    }
+
+    window.onscroll = function () { 
+        if (window.pageYOffset >= 4 * ((window.innerWidth * .4) / 6) + (0.005 * window.innerWidth)) {
+            navBottom.classList.add("sticky")
+            console.log("yes")
+        }
+        else {
+            navBottom.classList.remove("sticky")
+            console.log("no")
+        }
     }
 })
